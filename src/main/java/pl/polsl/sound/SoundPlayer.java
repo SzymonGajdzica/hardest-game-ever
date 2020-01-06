16ed536f8022a71be019bea91cf5c4f8dface855
SzGajdzica <sz.gajdzica@labmatic.com>
2020-01-06 12:30:29 +0100

    fixed sounds after jar generation

--- src/main/java/pl/polsl/sound/SoundPlayer.java
+++ src/main/java/pl/polsl/sound/SoundPlayer.java
@@ -5,7 +5,11 @@ import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 import pl.polsl.game.GameView;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,6 +19,13 @@ public class SoundPlayer {
     private final GameView gameView;
     private final Map<Sound, MediaPlayer> mediaPlayers = new HashMap<>();
     private static final int isTrue = 0;
+
+    private static FileSystem fileSystem;
+    private static FileSystem getFileSystem(String path) throws IOException {
+        if(fileSystem == null)
+            fileSystem = FileSystems.newFileSystem(URI.create(path), new HashMap<>());
+        return fileSystem;
+    }
 
     public SoundPlayer(GameView gameView) {
         this.gameView = gameView;
@@ -29,7 +40,7 @@ public class SoundPlayer {
                 return;
             }
             URL resource = SoundPlayer.class.getResource("/" + sound.getFileName());
-            MediaPlayer mediaPlayer = new MediaPlayer(new Media(Paths.get(resource.toURI()).toFile().toURI().toString()));
+            MediaPlayer mediaPlayer = new MediaPlayer(new Media(getPath(resource)));
             mediaPlayer.play();
             mediaPlayers.put(sound, mediaPlayer);
             mediaPlayer.setOnEndOfMedia(() -> {
@@ -42,6 +53,15 @@ public class SoundPlayer {
         } catch (Exception e) {
             gameView.notifyError(e);
         }
+    }
+
+    private String getPath(URL resource) throws Exception {
+        if(resource.toString().contains(".jar!")){
+            String[] array = resource.toString().split("!");
+            FileSystem fileSystem = getFileSystem(array[0]);
+            return fileSystem.getPath(array[1]).toUri().toString();
+        } else
+            return Paths.get(resource.toURI()).toFile().toURI().toString();
     }
 
 }
